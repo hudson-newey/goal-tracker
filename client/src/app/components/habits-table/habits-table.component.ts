a1ffc9f58f1a5ed50a684c333752e79f595e6e9a
hudson-newey <hudsonnewey@gmail.com>
2026-01-03 18:20:25 +1000

    Signal based habits table

--- client/src/app/components/habits-table/habits-table.component.ts
+++ client/src/app/components/habits-table/habits-table.component.ts
@@ -1,4 +1,11 @@
-import { ChangeDetectionStrategy, Component, input, output } from "@angular/core";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  input,
+  output,
+  signal,
+} from "@angular/core";
 import { Habit } from "src/app/models/habit";
 import { RouterLink } from "@angular/router";
 import { NoContextMenuDirective } from "../../directives/no-context-menu.directive";
@@ -12,18 +19,20 @@ import { NoContextMenuDirective } from "../../directives/no-context-menu.directi
 })
 export class HabitsTableComponent {
   public readonly models = input.required<Habit[]>();
-
   public readonly changeState = output<Habit>();
-
   public readonly decrementState = output<Habit>();
 
-  protected showAntiHabits = false;
+  protected readonly showAntiHabits = signal(false);
 
-  protected get positiveHabits(): Habit[] {
-    return this.models().filter((habit) => !habit.AntiHabit);
-  }
+  protected readonly positiveHabits = computed(() =>
+    this.models().filter((habit) => !habit.AntiHabit),
+  );
 
-  protected get antiHabits(): Habit[] {
-    return this.models().filter((habit) => habit.AntiHabit);
+  protected readonly antiHabits = computed(() =>
+    this.models().filter((habit) => habit.AntiHabit),
+  );
+
+  protected toggleAntiHabits(): void {
+    this.showAntiHabits.update((currentValue) => !currentValue);
   }
 }
